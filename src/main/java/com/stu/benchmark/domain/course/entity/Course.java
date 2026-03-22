f76d8c776ea76d6f142d26b907ab936624faf752
Sailor-Ham (Mac mini) <sangja234@gmail.com>
2026-03-22 17:51:42 +0900

    ✨ feat(course): Course Entity 작성 및 Repository 작성 #2
    - 수강인원(enrolledCount) 증가하는 핵심 비즈니스 로직 메서드 작성

---
+++ src/main/java/com/stu/benchmark/domain/course/entity/Course.java
@@ -0,0 +1,67 @@
+package com.stu.benchmark.domain.course.entity;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Entity
+@DynamicInsert
+@DynamicUpdate
+@Table(name = "course")
+@FieldDefaults(level = lombok.AccessLevel.PRIVATE)
+@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
+public class Course {
+
+	@Id
+	@Column(name = "course_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	Long id;
+
+	@Column(nullable = false, length = 100)
+	String title;
+
+	@Column(nullable = false)
+	Long maxCapacity;
+
+	@Column(nullable = false)
+	Long enrolledCount;
+
+	@CreationTimestamp
+	@Column(nullable = false, updatable = false)
+	LocalDateTime createdAt;
+
+	@UpdateTimestamp
+	@Column(nullable = false)
+	LocalDateTime updatedAt;
+
+	@Builder
+	public Course(String title, Long maxCapacity) {
+		this.title = title;
+		this.maxCapacity = maxCapacity;
+		this.enrolledCount = 0L;
+	}
+
+	public void increaseEnrolledCount() {
+
+		if (this.enrolledCount >= this.maxCapacity) {
+			throw new IllegalStateException("수강 인원이 초과되었습니다.");
+		}
+
+		this.enrolledCount++;
+	}
+}
